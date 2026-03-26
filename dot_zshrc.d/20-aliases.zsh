054d91b5789f811f9b6c35482d19e8452648d344
allig4t0r <allig4t0r@gmail.com>
2026-03-26 15:39:47 +0300

    upd aliases, add grouping, ttf for tf task

--- dot_zshrc.d/20-aliases.zsh
+++ dot_zshrc.d/20-aliases.zsh
@@ -1,32 +1,42 @@
+# brew
 alias bu="brew upgrade --greedy-latest"
 alias brwe=brew
-alias cat="bat --theme auto:system --theme-dark default --theme-light GitHub"
+
+# random
 alias dev="cd ~/repos"
 alias docs="cd ~/Documents"
 alias editghost="vim ~/.config/ghostty/config"
 alias editstarship="vim ~/.config/starship.toml"
-alias ch=chezmoi
+alias k=kubectl
+
+# system
+alias cat="bat --theme auto:system --theme-dark default --theme-light GitHub"
+alias mkdir='mkdir -pv'
+alias la="eza -la"
+alias ll="eza -l"
+alias ls=eza
+
+# git
+alias g=git
 alias ga="git add"
 alias gc="git commit -m"
 alias gp="git push"
 alias gs="git status"
-alias k=kubectl
-alias la="eza -la"
-alias ll="eza -l"
-alias ls=eza
+alias gl=serie
+alias lg=lazygit
+alias diff=difft
+
+# terraform
 alias tf=terraform
 alias tg=terragrunt
+alias ttf="task tf"
 
+# chezmoi
+alias ch=chezmoi
 alias chz="chezmoi apply && exec zsh"
 alias cha="chezmoi edit --apply"
 alias chp="chezmoi git push"
 alias chc="chezmoi -- git commit -m"
-
-alias g=git
-alias mkdir='mkdir -pv'
-alias gl=serie
-alias lg=lazygit
-alias diff=difft
 
 # Delete all remote tracking Git branches where the upstream branch has been deleted
 alias git_prune="git fetch --prune && git branch -vv | grep 'origin/.*: gone]' | awk '{print \$1}' | xargs git branch -d"
